b1635dc968621429d7f8900bd93c1a7226aa9dcf
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-02-09 19:53:13 +0500

    basic structure

--- project.cpp
+++ project.cpp
@@ -1,6 +1,72 @@
 #include<iostream>
 using namespace std;
 
+class MenuItem{
+    protected:
+        string id, name, category;
+        int price;
+    public:
+        void displayItem(){
+
+        }    
+};
+
+class Menu{
+    public:
+        void addItem(){
+
+        }
+        void removeItem(){
+
+        }
+        void displayMenu(){
+            
+        }
+        void getItemByID(){
+
+        }
+};
+
+class User{
+    protected:
+        string username, password;
+    public:
+        void login(){
+
+        }   
+        void logout(){
+
+        } 
+};
+
+class Customer : public User{
+    protected:
+        string orderHistory;
+    public:
+        void placeOrder(){
+
+        }   
+        void viewOrderHistory(){
+
+        } 
+};
+
+class Admin : public User{
+    public:
+        void addMenuItem(){
+
+        }
+        void removeMenuItem(){
+
+        }
+        void modifyMenuItem(){
+
+        }
+        void viewAllOrders(){
+            
+        }
+};
+
 int main(){
     
 }
